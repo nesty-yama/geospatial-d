c9ff1f3915f2c155f5e5d933ee7a025a8057ddec
土田 太陽 <taiyo-tsuchida@nesty-g.co.jp>
2022-02-12 16:54:10 +0900

    住所検索機能実装 & 表示切替UI途中まで

--- index.js
+++ index.js
@@ -38,3 +38,67 @@ SAITAMA_Shelter_GeoJSON.features.forEach(
 // // <!-- 埼玉県長瀞町_道路及び信号機 -->
 // L.geoJson(SAITAMA_Nagatoro_Road_GeoJSON).addTo(map);
 // L.geoJson(SAITAMA_Nagatoro_Signal_GeoJSON).addTo(map);
+
+
+
+
+
+// 表示座標を移動
+function inputChange(){
+  // 対象のinput要素.
+  let addressInput = document.getElementById('address');
+  let address = addressInput.value;
+
+  // 緯度経度からAPIで住所を検索する
+  getLatLngByAdreessForOpenStreetMap(address).then(latlng => {
+
+      let lat = latlng.lat;
+      let lng = latlng.lng;
+
+      // 取得した緯度・経度が不正なら何もしない
+      if(lat === 0 || lng === 0) return false;
+
+      // 表示座標をセット
+      map.setView([lat, lng]);
+  });
+}
+
+//------------------------------------//
+// 住所から緯度経度を返す OpenStreetMap
+// address:住所
+//------------------------------------//
+const getLatLngByAdreessForOpenStreetMap = (address) =>
+  new Promise((resolve, reject) => {
+    // 地図情報を取得するAPIを呼び出し
+    let result;
+    let params = {
+        format:"json"// 返り値の形式
+      , q:address    // 検索ワード
+    }
+    /*Baseの場合はJSONPに対応していないため、jqueryでajax返す*/
+    jQuery.ajax({
+          url:"https://nominatim.openstreetmap.org" // APIのURL
+        , data:params //パラメータ
+        , dataType:"json"
+        // 成功時
+        , success:function(data){
+          // 経度・緯度変数を初期化
+          let lat = 0;
+          let lon = 0;
+          // 検索結果があれば１件目をセット
+          if (data.length > 0) {
+            lat = data[0].lat;
+            lon = data[0].lon;
+          }
+          // leafletの経度・緯度オブジェクトにセットして返す
+          let latlng = L.latLng(lat, lon);
+          resolve(latlng);
+        }
+        // エラー時
+        , error: function(XMLHttpRequest, textStatus, errorThrown) {
+          console.log("XMLHttpRequest : " + XMLHttpRequest.status); // HTTPリクエストのステータス
+          console.log("textStatus : " + textStatus); // タイムアウト、パースエラー等の情報
+          console.log("errorThrown : " + errorThrown.message); // 例外情報
+        }
+    });
+  });
